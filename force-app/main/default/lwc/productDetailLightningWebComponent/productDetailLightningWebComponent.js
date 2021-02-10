55b382bb80006b99a62417b32c636183468f17ce
Ognjen Vuceljic <ognjen.vuceljic@namics.com>
2021-02-10 23:40:23 +0100

    feat(picnic): lwc logic updated

--- force-app/main/default/lwc/productDetailLightningWebComponent/productDetailLightningWebComponent.js
+++ force-app/main/default/lwc/productDetailLightningWebComponent/productDetailLightningWebComponent.js
@@ -1,3 +1,17 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, api } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
 
-export default class ProductDetailLightningWebComponent extends LightningElement {}
+export default class ProductDetailLightningWebComponent extends LightningElement {
+    @api recordId;
+    @wire (getRecord, {
+        recordId:'$recordId',
+        fields:[
+            'Product2.Name', 
+            'Product2.GTIN__c', 
+            'Product2.Purchasing_Price__c', 
+            'Product2.Selling_Price__c',
+            'Product2.Margin__c'
+        ]
+    })
+    product;
+}
